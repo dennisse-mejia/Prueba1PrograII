7a3f26d60a726318ba8e1fa8a501d09be8d1215b
Dennisse Mejia <dennissemejia@unitec.edu>
2024-07-25 16:35:17 -0600

    Cambios Clase RentItem

--- Prueba1/src/RentItem.java
+++ Prueba1/src/RentItem.java
@@ -12,16 +12,55 @@ public class RentItem {
     //Atributos 
     private String codigoItem;
     private String nombreItem;
-    private double precioRenta;
+    private double precioItem;
     private int cantidadCopias;
 
-    public RentItem(String codigoItem, String nombreItem, double precioRenta) {
+    public RentItem(String codigoItem, String nombreItem, double precioItem) {
         this.codigoItem = codigoItem;
         this.nombreItem = nombreItem;
-        this.precioRenta = precioRenta;
+        this.precioItem = precioItem;
         this.cantidadCopias = 0;
     }
-    
-    
+
+    //Funcion toString
+    public String toString() {
+        return "Código: " + codigoItem + "\nNombre: " + nombreItem + "\nPrecio Renta L." + precioItem + "\nCantidad Copias: " + cantidadCopias;
+    }
+
+    //Funcion pagoRenta
+    public double pagoRenta(int dias) {
+        return 0;
+    }
+
+    //Gettters para los 4 atributos
+    public String getCodigoItem() {
+        return codigoItem;
+    }
+
+    public String getNombreItem() {
+        return nombreItem;
+    }
+
+    public double getPrecioItem() {
+        return precioItem;
+    }
+
+    public int getCantidadCopias() {
+        return cantidadCopias;
+    }
+
+    //Funcion subMenu
+    public String subMenu() {
+        return "1-Imprimir";
+    }
+
+    //Funcion ejecutarOpcion
+    public void ejecutarOpcion(int opcion) {
+        if (opcion == 1) {
+            System.out.println(this.toString());
+        } else {
+            System.out.println("Opción No Válida");
+        }
+    }
 
 }
